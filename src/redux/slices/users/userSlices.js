13ae97a8874b476f3f7d1c39b6543a09eb45d908
Mighty Mouse <daikadai96@gmail.com>
2021-09-14 10:45:33 +0700

    7. User Registration Action

---
+++ src/redux/slices/users/userSlices.js
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//register action
+export const registerUserAction = createAsyncThunk(
+  'users/register',
+  async(user, {rejectWithValue, getState, dispatch }) => {
+    try {
+      //http call
+      const config = {
+        headers: {
+          'Content-Type':'application/json'
+        }
+      }
+      const { data } = await axios.post('http://localhost:5000/api/users/register', user, config)
+      return data
+    } catch (error) {
+      if(!error?.response) {
+        throw error
+      }
+      return rejectWithValue(error?.response?.data)
+    }
+  }
+)
