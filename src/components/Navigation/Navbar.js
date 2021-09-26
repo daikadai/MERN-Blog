7ce20d41c806f455e78a3d6be4ffaa9de6bba2ea
Mighty Mouse <daikadai96@gmail.com>
2021-09-26 13:06:06 +0700

    17. Display Navbar base on Login User

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,13 +1,23 @@
-import PublicNavbar from "./Public/PublicNavbar"
-import PrivateNavbar from "./Private/PrivateNavbar"
-import AdminNavbar from "./Admin/AdminNavbar"
+import PublicNavbar from "./Public/PublicNavbar";
+import PrivateNavbar from "./Private/PrivateNavbar";
+import AdminNavbar from "./Admin/AdminNavbar";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  //get user from store
+  const { userAuth } = useSelector((state) => state.users);
+  const isAdmin = userAuth?.isAdmin;
   return (
-    <div>
-      <AdminNavbar />
-    </div>
-  )
-}
+    <>
+      {isAdmin ? (
+        <AdminNavbar />
+      ) : userAuth ? (
+        <PrivateNavbar />
+      ) : (
+        <PublicNavbar />
+      )}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
